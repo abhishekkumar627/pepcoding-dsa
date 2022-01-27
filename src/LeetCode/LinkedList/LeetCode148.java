2153c41380eb22f773c78f18febd16d1a40b25e9
Abhishek Kumar <abhishek.kumar627@gmail.com>
2022-01-27 17:21:16 +0530

    Changes w.r.t Leetcode solutions and Contest 3 Questions

---
+++ src/LeetCode/LinkedList/LeetCode148.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head ==null || head.next == null){
+            return head;
+        }
+        ListNode mid = getMidNode(head);
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(mid);
+        return mergeTwoLists(l1,l2);
+    }
+
+    ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        ListNode dummyHead = new ListNode();
+        ListNode currentHead = dummyHead;
+        while (list1 != null && list2 != null){
+            if (list1.val < list2.val){
+                currentHead.next = list1;
+                list1 = list1.next;
+                currentHead = currentHead.next;
+            }else {
+                currentHead.next = list2;
+                list2 =list2.next;
+                currentHead = currentHead.next;
+            }
+        }
+        currentHead.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    } 
+
+    ListNode middleNode(ListNode head) {
+        ListNode midPrev = null;  
+        while(head != null && head.next != null){
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+         ListNode mid = midPrev.next;
+         midPrev.next = null;
+        return mid;
+    }
+}
---
+++ src/LeetCode/LinkedList/LeetCode21.java
@@ -0,0 +1,51 @@
+/** 21. Merge Two Sorted Lists
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+package LeetCode.LinkedList;
+import java.util.*;
+public class LeetCode21 { 
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {       
+        if(list1 == null) { 
+            return list2; 
+        } else if(list2 == null) {
+            return list1;
+        }        
+
+        ListNode sortedHead = new ListNode();
+        ListNode currentHead = sortedHead;       
+
+        while(list1 != null &&  list2 != null){
+            if(list1.val<list2.val){ 
+                currentHead.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                currentHead.next = list2;
+                list2 = list2.next;
+            }
+            currentHead = currentHead.next;
+        }
+
+        while(list1!=null){
+            currentHead.next = list1;
+            list1 = list1.next;
+            currentHead = currentHead.next;
+        }
+
+        while(list2!=null){
+            currentHead.next = list2;
+            list2 = list2.next;
+            currentHead = currentHead.next;
+        }
+        
+        return sortedHead.next;                
+    }
+}
